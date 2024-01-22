5f4381ec4858398e1ba71536a5defd2b24753a4c
Diego <Diego.iribe@outlook.es>
2024-01-22 05:43:34 -0700

    slide foto

--- src/Components/Launchpad/Main/Main.jsx
+++ src/Components/Launchpad/Main/Main.jsx
@@ -19,7 +19,7 @@ const Container = styled.div`
 `
 const Img = styled.div`
   width: 100%;
-  height: 100%;
+  height: 90%;
   box-shadow: rgba(0, 0, 0, 0.3) 0px 1px 11px 0px;
   border-radius: 10px;
   @media (max-width: 980px) {
@@ -28,21 +28,39 @@ const Img = styled.div`
   }
 `
 
-const Main = () => {
-  const [img, setImg] = React.useState(0)
+const DivSpan = styled.div`
+  display: flex;
+  gap: 0.5rem;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+`
 
-  const handleImg = () => {
-    setImg((prevImg) => (prevImg + 1) % 3)
+const Span = styled.span`
+  width: 30px;
+  height: 10px;
+  border-radius: 5px;
+`
+const Main = () => {
+  const [img, setImg] = useState(0)
+
+  const handleImg = (index) => {
+    if (index === img || index === 4) {
+      setImg((prevImg) => (prevImg + 1) % 3)
+    }
   }
 
   const getBackgroundImage = () => {
     switch (img) {
       case 0:
         return frente
+
       case 1:
         return lado
+
       case 2:
         return gorra
+
       default:
         return frente
     }
@@ -56,8 +74,22 @@ const Main = () => {
           backgroundPosition: 'center ',
           backgroundRepeat: 'no-repeat'
         }}
-        onClick={() => handleImg()}
+        onClick={() => handleImg(4)}
       ></Img>
+      <DivSpan>
+        <Span
+          style={{ background: img === 0 ? '#323232' : '#bcbdbb' }}
+          onClick={() => handleImg(2)}
+        ></Span>
+        <Span
+          style={{ background: img === 1 ? '#323232' : '#bcbdbb' }}
+          onClick={() => handleImg(0)}
+        ></Span>
+        <Span
+          style={{ background: img === 2 ? '#323232' : '#bcbdbb' }}
+          onClick={() => handleImg(1)}
+        ></Span>
+      </DivSpan>
     </Container>
   )
 }
